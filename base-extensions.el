40286a50b4299ffe92a4d93b35de61c41404f515
Artem Kovalov <artem.kovalyov@gmail.com>
2020-10-21 15:10:37 +0200

    Add MDX to Markdown

--- base-extensions.el
+++ base-extensions.el
@@ -276,6 +276,7 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.mdx\\'" . markdown-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
