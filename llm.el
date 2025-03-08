1d9c3502358811d329894518e6ccffb4b73c0df5
Artem Kovalyov <artem.kovalyov@gmail.com>
2025-03-08 10:43:02 +0100

    Add Ellama

---
+++ llm.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+;;; package --- ellama config
+;;; Commentary:
+;;;
+
+;;; Code:
+
+
+(use-package ellama
+  :straight (ellama :type git :host github :repo "s-kostyaev/ellama"))
